08dde01a55f6b051abaabaac7da9f45a344b3cda
morganhah99 <mjh58@njit.edu>
2021-12-01 21:40:55 -0500

    this is the most working build, pytest, pylint, pandas work here

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -1,12 +1,19 @@
 """Testing Addition"""
 from calc.calculations.addition import Addition
+import pandas as pd
+import os
 
+outname = '10addition.csv'
+
+outdir = r'C:\Users\imabe\PycharmProjects\calc3\input\10addition.csv'
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+
+fullname = os.path.join(outdir, outname)
+
+df.to_csv(fullname)
+df = pd.read_csv(r'C:\Users\imabe\PycharmProjects\calc3\input\10addition.csv')
+print(df)
 
 def test_calculation_addition():
     """testing that our calculator has a static method for addition"""
-    # Arrange
-    my_numbers = (1.0, 2.0)
-    addition = Addition(my_numbers)
-    # Act
-    # Assert
-    assert addition.get_result() == 3.0
