6a4f6b9ebbb7e9c136d89137fe7f1c3171e1c78a
morganhah99 <mjh58@njit.edu>
2021-12-01 22:27:03 -0500

    added absolutepath method

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -1,19 +1,10 @@
 """Testing Addition"""
 from calc.calculations.addition import Addition
 import pandas as pd
-import os
+from Fileutilities.absolutepath import absolutepath
+path = absolutepath(r'C:\Users\imabe\PycharmProjects\calc3\input\10addition.csv')
 
-outname = '10addition.csv'
-
-outdir = r'C:\Users\imabe\PycharmProjects\calc3\input\10addition.csv'
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
-
-fullname = os.path.join(outdir, outname)
-
-df.to_csv(fullname)
-df = pd.read_csv(r'C:\Users\imabe\PycharmProjects\calc3\input\10addition.csv')
-print(df)
-
+data = pd.read_csv(path)
+print(data)
 def test_calculation_addition():
     """testing that our calculator has a static method for addition"""
