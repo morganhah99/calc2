aeeb910dbace7fcde1c4bdfb6fab5d994e43f2f9
morganhah99 <mjh58@njit.edu>
2021-12-08 18:14:25 -0500

    works right now with form validation and flashes

--- app/app.py
+++ app/app.py
@@ -2,14 +2,33 @@
 from flask import Flask, request
 from flask import render_template
 from flask import flash
+from flask import redirect
+from flask import url_for
 from calc.calculator import Calculator
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/")
 def index():
     """index  Route Response"""
-    flash("hi")
+    return render_template('index.html')
+
+@app.route("/home")
+def home():
+    """index home response"""
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'secret':
+            error = 'Invalid credentials'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
 
 @app.route("/basicform", methods=['GET', 'POST'])
 def basicform():
